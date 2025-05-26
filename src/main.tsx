c015608217ff67e26af76e24a1837344e8a49860
souzafred <frd.sza@gmail.com>
2025-05-26 19:55:29 -0300

    ok

---
+++ src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SalesIntelligenceDashboard from './sales-intelligence-dashboard';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SalesIntelligenceDashboard />
+  </React.StrictMode>
+);
